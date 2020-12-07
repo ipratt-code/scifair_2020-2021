08ba641a4a44f2b74fdf120cfb942978650de302
Ian Pratt <ianpratt@pop-os.localdomain>
2020-12-07 12:46:03 -0600

    change makefile

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ run: install_prereq
 	python3 $(CURDIR)/the-actual-code/disease_model.py
 
 install_prereq: $(CURDIR)/requirements.txt
-	python3 -mpip install -r $(CURDIR)/requirements.txt --force-reinstall --no-cache-dir
+	python3 -mpip install -U -r $(CURDIR)/requirements.txt --no-cache-dir
--- the-actual-code/disease_model.py
+++ the-actual-code/disease_model.py
@@ -13,16 +13,20 @@ Infection_period = 14  # infection period
 Incubation_period = 0.001  # incubation period
 days_till_death = 14  # days untill someone dies from disease
 starting_infected = 1  # how many people start infected
+starting_dead = 0  # how many people start dead
+alpha = 0.3  # death rate
 how_long = 2000  # how many days for simulation to run
 R_0 = 2.22  # R naught
+
+# These are the variables that we will tweak with lmfit
 mask_effectiveness_in = 0.50  # in percentages
 mask_effectiveness_out = 0.50  # in percentages
 compliance = 0.50  # in percentages
 
+# these variables are the ones that we put into the models
 gamma = 1.0 / Infection_period  # infection period
 delta = 1.0 / Incubation_period  # incubation period
 beta = R_0 * gamma  # R_0 = beta / gamma, so beta = R_0 * gamma. Its the infectivity
-alpha = 0.3  # death rate
 rho = 1 / days_till_death  # days from infection until death
 epsilon = (1 - mask_effectiveness_in * compliance) * (
     1 - mask_effectiveness_out * compliance
@@ -35,7 +39,7 @@ The math behind this can be found at https://github.com/aatishb/maskmath/blob/ma
 """
 
 
-def deriv(y, t, N, beta, gamma, delta, alpha, rho, full_output=1):
+def deriv(y, t, N, beta, gamma, delta, alpha, rho, epsilon, full_output=1):
     S, E, I, R, D = y
     dSdt = -beta * S * I * epsilon / N
     dEdt = beta * S * I * epsilon / N - delta * E
@@ -46,17 +50,17 @@ def deriv(y, t, N, beta, gamma, delta, alpha, rho, full_output=1):
 
 
 S0, E0, I0, R0, D0 = (
-    N - starting_infected,
+    N - starting_infected - starting_dead,
     0,
     starting_infected,
     0,
-    0,
+    starting_dead,
 )
 # initial conditions:
 # one infected, rest
 # susceptible
 
-t = np.linspace(0, how_long, 1000)  # Grid of time points (in days)
+t = np.linspace(0, how_long, how_long)  # Grid of time points (in days)
 # NOTE: the last variable num is how granular it
 # will be when you integrate the derivatives.
 # preference is to set it equal to how_long
@@ -64,12 +68,14 @@ t = np.linspace(0, how_long, 1000)  # Grid of time points (in days)
 y0 = S0, E0, I0, R0, D0  # Initial conditions vector
 
 # Integrate the SIR equations over the time grid, t.
-ret = odeint(deriv, y0, t, args=(N, beta, gamma, delta, alpha, rho))
+ret = odeint(deriv, y0, t, args=(N, beta, gamma, delta, alpha, rho, epsilon))
 S, E, I, R, D = ret.T
 
 data_array = [S, E, I, R, D]
 
-np.savetxt(disease_name + "~" + str(N) + ".csv", data_array, delimiter=",", fmt="%d")
+np.savetxt(
+    disease_name + "-pop_size:" + str(N) + ".csv", data_array, delimiter=",", fmt="%d"
+)
 
 # Making the plot
 f, ax = plt.subplots(1, 1, figsize=(10, 4))
@@ -83,10 +89,10 @@ ax.plot(t, I, "r", alpha=0.7, linewidth=2, label="Infected")
 ax.plot(t, R, "g", alpha=0.7, linewidth=2, label="Recovered")
 ax.plot(t, D, "k", alpha=0.7, linewidth=2, label="Dead")
 
-ax.set_title("Affect of " + disease_name + " on a population of " + str(N))
+ax.set_title("Effect of " + disease_name + " on a population of " + str(N))
 
 ax.set_xlabel("Time (days)")
-ax.set_ylabel("Number of people")
+ax.set_ylabel("Number of people affected")
 
 
 ax.yaxis.set_tick_params(length=0)
