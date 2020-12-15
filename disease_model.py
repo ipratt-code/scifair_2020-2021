e9e6c6f0aca8c7e636fed0c50d5b4086e2939674
Ian Pratt <Thingcoder1@gmail.com>
2020-12-15 10:05:23 -0600

    finishing up the model

--- disease_model.py
+++ disease_model.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+# from operator import itemgetter
+
 pd.options.mode.chained_assignment = None  # default='warn'
 
 import matplotlib.pyplot as plt
@@ -8,11 +10,14 @@ import matplotlib.dates as mdates
 
 from scipy.integrate import odeint
 import lmfit
-from lmfit.model import save_modelresult, load_modelresult
+
+# from lmfit.model import save_modelresult, load_modelresult
 
 import warnings
 import yaml
 from pathlib import Path
+from sklearn.metrics import r2_score
+
 
 warnings.filterwarnings("ignore")
 
@@ -23,6 +28,7 @@ with open(config_path, "r") as conf:
     conf.close()
 
 disease = config["train"]["disease_name"]
+test_disease = config["test"]["disease_name"]
 
 model = Path.cwd() / "models" / config["model_file"]
 
@@ -36,16 +42,19 @@ Infection_period = mod_data["train"][disease]["fixed_parameters"]["infection_per
 Incubation_period = mod_data["train"][disease]["fixed_parameters"]["incubation_period"]
 alpha = mod_data["train"][disease]["fixed_parameters"]["death_rate"]
 days_till_death = mod_data["train"][disease]["fixed_parameters"]["days_until_death"]
+how_long = mod_data["train"]
+
+country_name = mod_data["model_constants"]["name"]
 
 # read the data
-agegroups = pd.read_csv("data/agegroups.csv")
-probabilities = pd.read_csv("data/probabilities.csv")
+agegroups = pd.read_csv(Path("data/agegroups.csv"))
+# probabilities = pd.read_csv("data/probabilities.csv")
 covid_data = pd.read_csv(
-    "data/time_series_covid19_deaths_global_narrow.csv",
-    parse_dates=["Date"],
-    skiprows=[1],
+    Path("data/owid-covid-data.csv"),
+    # parse_dates=["date"],
+    # skiprows=[1],
 )
-covid_data["Location"] = covid_data["Country/Region"]
+# covid_data["Location"] = covid_data["Country/Region"]
 
 # create some dicts for fast lookup
 # 1. agegroups
@@ -71,17 +80,14 @@ agegroup_lookup = dict(
 
 starting_infected = 1  # how many people start infected
 starting_dead = 0  # how many people start dead
-how_long = 2000  # how many days for simulation to run
 
 # these variables are the ones that we put into the models
 gamma = 1.0 / Infection_period  # infection period
 delta = 1.0 / Incubation_period  # incubation period
 rho = 1 / days_till_death  # days from infection until death
 
-plt.gcf().subplots_adjust(bottom=0.15)
 
-
-def plotter(t, S, E, I, R, D, R_0, B, S_1=None, S_2=None, x_ticks=None):
+def plotter(t, S, E, I, R, D, R_0, S_1=None, S_2=None, x_ticks=None):
 
     f, ax = plt.subplots(1, 1, figsize=(20, 4))
     if x_ticks is None:
@@ -109,8 +115,6 @@ def plotter(t, S, E, I, R, D, R_0, B, S_1=None, S_2=None, x_ticks=None):
     legend.get_frame().set_alpha(0.5)
     for spine in ("top", "right", "bottom", "left"):
         ax.spines[spine].set_visible(False)
-
-    plt.show()
 
     f = plt.figure(figsize=(20, 4))
 
@@ -229,6 +233,11 @@ def double_logistic_R_0(
 def Model(
     days,
     population_size,
+    gamma,
+    delta,
+    rho,
+    alpha,
+    R_0,
     R_0_start_mult,
     k1,
     x01,
@@ -271,23 +280,17 @@ def Model(
     S, E, I, R, D = ret.T
     R_0_over_time = [beta(i) / gamma for i in range(len(t))]
 
-    return (
-        t,
-        S,
-        E,
-        I,
-        R,
-        D,
-        R_0_over_time,
-        compliance,
-        mask_effectiveness_out,
-        mask_effectiveness_in,
-    )
+    return (t, S, E, I, R, D, R_0_over_time)
 
 
 # parameters
-data = covid_data[covid_data["Location"] == "Italy"]["Value"].values[::-1]
-population_size = sum(agegroup_lookup["Italy"])
+data = covid_data[covid_data["location"] == country_name][
+    "total_deaths"
+]  # .values[::-1]
+try:
+    population_size = sum(agegroup_lookup[country_name])
+except:
+    population_size = 30000000
 # beds_per_100k = beds_lookup["Italy"]
 outbreak_shift = 30
 params_init_min_max = {
@@ -322,6 +325,11 @@ def fitter(
     ret = Model(
         days,
         population_size,
+        gamma,
+        delta,
+        rho,
+        alpha,
+        R_0,
         R_0_start_mult,
         k1,
         x01,
@@ -336,7 +344,19 @@ def fitter(
     return ret[5][x]
 
 
-def fit(fitter, days, data, population_size, params_init_min_max, model_file):
+def fit(
+    fitter,
+    days,
+    data,
+    population_size,
+    params_init_min_max,
+    gamma,
+    delta,
+    rho,
+    alpha,
+    R_0,
+    model_file,
+):
 
     if outbreak_shift >= 0:
         y_data = np.concatenate((np.zeros(outbreak_shift), data))
@@ -363,15 +383,37 @@ def fit(fitter, days, data, population_size, params_init_min_max, model_file):
     result.plot_fit(datafmt="-")
 
     full_days = 500
-    first_date = np.datetime64(covid_data.Date.min()) - np.timedelta64(
-        outbreak_shift, "D"
-    )
+    first_date = np.datetime64(covid_data.date.min())  # - np.timedelta64(
+    #    outbreak_shift, "D"
+    # )
     x_ticks = pd.date_range(start=first_date, periods=full_days, freq="D")
+
+    modPredictScore = Model(
+        len(y_data),
+        population_size,
+        gamma,
+        delta,
+        rho,
+        alpha,
+        R_0,
+        **result.best_values,
+    )
+
+    pred_y_data = modPredictScore[5]
+
+    r2 = float(r2_score(y_data, pred_y_data))
+
+    print("The accuracy (R2 metric) for this model is: ", r2)
 
     plotter(
         *Model(
             full_days,
             population_size,
+            gamma,
+            delta,
+            rho,
+            alpha,
+            R_0,
             **result.best_values,
         ),
         x_ticks=x_ticks,
@@ -380,21 +422,98 @@ def fit(fitter, days, data, population_size, params_init_min_max, model_file):
     with open(model_file, "w") as file:
         for param in result.best_values:
             mod_data["flexible_params"][param] = float(result.best_values[param])
+        mod_data["accuracy"] = r2
         yaml.dump(mod_data, file)
     # return result.best_values
 
 
-def predict(days, population_size, params_init_min_max, model_file):
+def predict(model_file):
     with open(model_file, "r") as f:
         mod = yaml.load(f, Loader=yaml.FullLoader)
+    flex_params = {
+        "R_0_start_mult": 0,
+        "k1": 0,
+        "x01": 0,
+        "R_0_middle_mult": 0,
+        "k2": 0,
+        "x02": 0,
+        "R_0_end_mult": 0,
+        "mask_effectiveness_in": 0,
+        "mask_effectiveness_out": 0,
+        "compliance": 0,
+    }
+
+    days = mod["model_constants"]["how_many_days"]
+
+    disease_params = mod["test"][test_disease]
+    for param in mod["flexible_params"]:
+        flex_params[param] = mod["flexible_params"][param]
+
+    R_0 = disease_params["R_0"]
+    Infection_period = disease_params["infection_period"]
+    Incubation_period = disease_params["incubation_period"]
+    alpha = disease_params["death_rate"]
+    days_till_death = disease_params["days_until_death"]
+
+    # population_size = population_size
+
+    # these variables are the ones that we put into the models
+    gamma = 1.0 / Infection_period  # infection period
+    delta = 1.0 / Incubation_period  # incubation period
+    rho = 1 / days_till_death  # days from infection until death
+
+    # print(flex_params)
+    # print(disease_params)
+
     plotter(
         *Model(
             days,
             population_size,
-            **mod["flexible_params"],
+            gamma,
+            delta,
+            rho,
+            alpha,
+            R_0,
+            **flex_params,
         ),
-        x_ticks=days,
+        x_ticks=None,
     )
 
 
-fit(fitter, days, data, population_size, params_init_min_max, model)
+def askFitOrPredict():
+    inp = input(
+        "Please type fit (f) or predict (p) depending on what you want to do. \n\
+The config.yml file has an incorrect input for the option fit_or_predict \
+and therefore requrires user input.\n[Fit(f)/Predict(p)]:"
+    )
+    if inp == "fit" or inp == "f":
+        return "fit"
+    elif inp == "predict" or inp == "p":
+        return "predict"
+    elif inp != "fit" or inp != "f" or inp != "predict" or inp != "p":
+        print("That input was not recognised. Please try again.\n")
+        askFitOrPredict()
+
+
+def fitOrPredict(uinput):
+    if uinput == "fit":
+        fit(
+            fitter,
+            days,
+            data,
+            population_size,
+            params_init_min_max,
+            gamma,
+            delta,
+            rho,
+            alpha,
+            R_0,
+            model,
+        )
+    elif uinput == "predict":
+        predict(model)
+    elif uinput != "fit" and uinput != "predict":
+        fitOrPredict(askFitOrPredict())
+
+
+fitOrPredict(config["fit_or_predict"])
